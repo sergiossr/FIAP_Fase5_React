2908a2b7d39973871a9625579a5d74645b303bfd
SergioSSR <sergio.ssr1@gmail.com>
2023-05-03 19:53:44 -0700

    new fix

--- src/Componentes/Sidebar/SidebarData.js
+++ src/Componentes/Sidebar/SidebarData.js
@@ -6,25 +6,25 @@ export const SidebarData = [
   {
     title: 'Home',
     path: '/home',
-    icon: <AiIcons.AiOutlineHome  fontSize="3rem" color='black'/>,
+    icon: <AiIcons.AiOutlineHome  fontSize="2rem" color='black'/>,
     cName: 'nav-text'
   },
   {
     title: 'Documentos',
     path: '/documentos',
-    icon: <AiIcons.AiOutlineFolder fontSize="3rem" color='black'/>,
+    icon: <AiIcons.AiOutlineFolder fontSize="2rem" color='black'/>,
     cName: 'nav-text'
   },
   {
     title: 'Perfil',
     path: '/perfil',
-    icon: <FaIcons.FaRegAddressBook fontSize="3rem" color='black'/>,
+    icon: <FaIcons.FaRegAddressBook fontSize="2rem" color='black'/>,
     cName: 'nav-text'
   },
   {
     title: 'Logout',
     path: '/',
-    icon: <FaIcons.FaSignOutAlt fontSize="3rem" color='black'/>,
+    icon: <FaIcons.FaSignOutAlt fontSize="2rem" color='black'/>,
     cName: 'nav-text'
   }
 ];
--- src/Componentes/TicketTable/TicketTable.js
+++ src/Componentes/TicketTable/TicketTable.js
@@ -19,7 +19,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     color: theme.palette.common.white,
   },
   [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
+    fontSize: 10,
   },
 }));
 
